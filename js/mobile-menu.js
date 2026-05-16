b3f01e4d4d61f77977700400a719ddf80c04aefc
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2026-05-16 10:28:57 +0300

    1

---
+++ js/mobile-menu.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const navListRef = document.querySelector(".p-q-navigation-list");
+  const headerRef = document.querySelector(".p-q-header");
+  const navigationRef = document.querySelector(".p-q-navigation");
+
+  menuBtnRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
+    navListRef.classList.toggle("is-open");
+    headerRef.classList.toggle("is-open");
+    navigationRef.classList.toggle("is-open");
+  });
+
+  document.querySelectorAll(".p-q-navigation-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      navListRef.classList.remove("is-open");
+      menuBtnRef.classList.remove("is-open");
+      headerRef.classList.remove("is-open");
+      navigationRef.classList.remove("is-open");
+    }),
+  );
+});
